99f25e11a4422d2d9da96b47582705d45106575e
MarcoGHL <79898179+MarcoGHL@users.noreply.github.com>
2021-04-20 15:24:39 +0200

    created MainViewModel and added DataContext

---
+++ PinderMVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandHelper;
+using System.ComponentModel;
+
+namespace PinderMVVM.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void notifyPropertyChanged(string propname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+
+    }
+}
