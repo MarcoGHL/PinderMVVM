e847011a3b8c2033a0bd773aa8238952a9f55ce9
MarcoGHL <marco.guhl38@gmail@gmail.com>
2021-06-22 15:10:57 +0200

    created scan function

--- PinderMVVM/ViewModel/MainViewModel.cs
+++ PinderMVVM/ViewModel/MainViewModel.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-//using CommandHelper;
+using CommandHelper;
 using System.ComponentModel;
 using System.IO;
+using System.Windows.Input;
+using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace PinderMVVM.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private ICommand _scanCommand;
 
 
-        public List<string> Laufwerke { get; set; }
+        public ObservableCollection<string> DirectoryCollection { get; set; }
 
         private void notifyPropertyChanged(string propname)
         {
-            //Test
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
         }
 
         public MainViewModel()
         {
+            this.DirectoryCollection = new ObservableCollection<string>();
+            ScanDirectory();
+        }
+      
+        public ICommand ScanCommand
+        {
+            get
+            {
+                return _scanCommand = new RelayCommand(c => ScanDirectory());
+            }
+        }
+
+        private void ScanDirectory()
+        {
+            // get logical drives and clear the obCollection 
             string[] drives = Directory.GetLogicalDrives();
-            Laufwerke = new List<string>(drives);
-            Laufwerke.Add(Properties.Settings.Default.CurrPath);
+            DirectoryCollection.Clear();
+
+            // go through drives and add them to the obCollection
+            foreach (string directory in drives)
+            {
+                if (!DirectoryCollection.Contains(directory))
+                {
+                    this.DirectoryCollection.Add(directory);
+                }
+            }
         }
 
     }
